53b43069e3cc968b79fc0e6a6b2472d211cffe70
ahmed <adlahmed512f@gmail.com>
2026-04-27 04:07:11 +0300

    hiiiiiiii

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,7 @@ public class Main {
         slider.addChangeListener(e -> button.setText("Value: " + slider.getValue()));
         button2.addActionListener(e -> {secondary_frame secondaryFrame = new secondary_frame();});
         frame.setVisible(true);
+
+        dahdkjasdhkjasdhjkas
     }
 }
